31595440d9c99055eb4e4bbe52c77b006ab66beb
SPSTVMCR <msupersteve@gmail.com>
2024-07-23 13:29:39 +0700

    Complete the header. Add the banner for MB and PC.

--- home/home.js
+++ home/home.js
@@ -17,3 +17,35 @@ window.addEventListener('scroll', function() {
   }
   currentScrollPos = window.scrollY;
 });
+
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function toggleDropdown() {
+  document.getElementById("productDropdown").classList.toggle("dropdown-content");
+  document.getElementById("productDropdown").classList.toggle("show");
+}
+
+//Icon dropdown transform 
+function iconDropdown() {
+  let rotated = document.getElementById("icon-dropdown").style.transform;
+  if (rotated === "rotate(-90deg)") {
+    document.getElementById("icon-dropdown").style.transform = "rotate(0deg)";
+  } else {
+    document.getElementById("icon-dropdown").style.transform = "rotate(-90deg)";
+  }
+  document.getElementById("icon-dropdown").style.transition = "transform 0.3s";
+}
+function toggleDropdownAccount() {
+  document.getElementById("accountDropdown").classList.toggle("dropdown-content");
+  document.getElementById("accountDropdown").classList.toggle("show");
+}
+function iconDropdownAccount() {
+  let scaled = document.getElementById("icon-dropdown-account").style.transform;
+  if (scaled === "scale(0.8)") {
+    document.getElementById("icon-dropdown-account").style.transform = "scale(1)";
+  } else {
+    document.getElementById("icon-dropdown-account").style.transform = "scale(0.8)";
+  }
+  document.getElementById("icon-dropdown-account").style.transition = "transform 0.3s";
+}
